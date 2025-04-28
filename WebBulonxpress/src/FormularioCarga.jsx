6c1c9f78d243e308b24b4369f566cf125746878d
Matt Ayala <mattayala_15@outlook.com>
2025-04-28 00:45:58 -0300

    Agregamos campo facturacion y comentario

--- WebBulonxpress/src/FormularioCarga.jsx
+++ WebBulonxpress/src/FormularioCarga.jsx
@@ -7,7 +7,9 @@ const FormularioCarga = ({ onAgregar }) => {
     cliente: '',
     factura: '',
     monto: '',
-    vencimiento: ''
+    facturacion:'',
+    vencimiento: '',
+    comentario:''
   });
 
   const handleChange = (e) => {
@@ -23,10 +25,12 @@ const FormularioCarga = ({ onAgregar }) => {
         cliente,
         factura: form.factura,
         monto: form.monto,
-        vencimiento: form.vencimiento
+        facturacion:form.facturacion,
+        vencimiento: form.vencimiento,
+        comentario: form.comentario
       });
       setClienteActual(cliente); // se fija definitivamente
-      setForm({ cliente: '', factura: '', monto: '', vencimiento: '' });
+      setForm({ cliente: '', factura: '', monto: '',facturacion:'', vencimiento: '',comentario:'' });
     }
   };
 
@@ -55,9 +59,11 @@ const FormularioCarga = ({ onAgregar }) => {
             </Button>
           </>
         )}
-        <TextField name="factura" label="Factura" value={form.factura} onChange={handleChange} />
+        <TextField name="factura" label="Comprobante" value={form.factura} onChange={handleChange} />
         <TextField name="monto" label="Monto" type="number" value={form.monto} onChange={handleChange} />
+        <TextField name="facturacion" label="Facturacion" type="date" InputLabelProps={{ shrink: true }} value={form.facturacion} onChange={handleChange} />
         <TextField name="vencimiento" label="Vencimiento" type="date" InputLabelProps={{ shrink: true }} value={form.vencimiento} onChange={handleChange} />
+        <TextField name="comentario" label="Comentario" value={form.comentario} onChange={handleChange} />
         <Button variant="contained" type="submit">Agregar</Button>
       </Stack>
     </form>
