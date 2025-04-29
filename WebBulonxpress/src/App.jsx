37a284b1352dae36e1f307bb3061a4055c85b10b
Matias <mattayala_15@outlook.com>
2025-04-29 08:32:48 -0300

    Arreglando bug de impresion

--- WebBulonxpress/src/App.jsx
+++ WebBulonxpress/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Container, Typography, Divider } from '@mui/material';
+import { Typography, Divider, Container } from '@mui/material';
 import FormularioCarga from './FormularioCarga';
 import TablaFacturas from './TablaFacturas';
 import BotonImprimir from './BotonImprimir';
 import './Cobranza.css';
 import './App.css';
 
-function Cobranza() {
+function App() {
   const [datos, setDatos] = useState([]);
 
   const agregarDato = (nuevo) => {
@@ -14,14 +14,16 @@ function Cobranza() {
   };
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>Cobranzas</Typography>
-      <FormularioCarga onAgregar={agregarDato} />
-      <Divider sx={{ my: 2 }} />
-      <TablaFacturas datos={datos} />
-      <BotonImprimir />
-    </Container>
+    <>
+      <Container>
+        <Typography variant="h4" gutterBottom>Cobranzas</Typography>
+        <FormularioCarga onAgregar={agregarDato} />
+        <Divider />
+        <TablaFacturas datos={datos} />
+        <BotonImprimir />
+      </Container>
+    </>
   );
 }
 
-export default Cobranza;
+export default App;
